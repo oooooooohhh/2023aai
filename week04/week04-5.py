d5a9e2094f1a8a70f8bac6b45f69360e95bdb135
oooooooohhh <145088537+oooooooohhh@users.noreply.github.com>
2023-10-06 12:17:08 +0800

    Add files via upload

---
+++ week04/week04-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+
+        ans=1
+        for n in nums:
+            ans *= n
+        if ans>0: return 1
+        if ans<0: return -1
+        if ans==0: return 0
