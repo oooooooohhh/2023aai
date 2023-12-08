e690f9a0008ee74ea4ffc238e48f231361259d78
oooooooohhh <145088537+oooooooohhh@users.noreply.github.com>
2023-12-08 11:43:06 +0800

    Add files via upload

---
+++ week13/week13-4c.py
@@ -0,0 +1,6 @@
+a=input()
+
+N = len(a)
+for i in range(N-1,-1,-1):
+    print( a[i], end='')
+print()
---
+++ week13/week13-5b.py
@@ -0,0 +1,7 @@
+return ''.join(word1) == ''.join(word2)
+        #a = ''.join(word1)
+        #b = ''.join(word2)
+
+
+        #if a==b: return True
+        #else: return False
